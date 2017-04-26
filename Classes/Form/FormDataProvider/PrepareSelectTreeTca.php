dedf8709ce7a421b413aa893691be6120e3895a4
Bastian Zagar <zagar@aixdesign.net>
2017-04-26 20:31:41 +0200

    Tca Generation

--- Classes/Form/FormDataProvider/PrepareSelectTreeTca.php
+++ Classes/Form/FormDataProvider/PrepareSelectTreeTca.php
@@ -1,19 +1,15 @@
 <?php
+
 namespace MageDeveloper\Dataviewer\Form\FormDataProvider;
 
+use MageDeveloper\Dataviewer\Domain\Model\Datatype;
 use MageDeveloper\Dataviewer\Domain\Model\Record;
-use MageDeveloper\Dataviewer\Domain\Repository\FieldRepository;
-use MageDeveloper\Dataviewer\Utility\LocalizationUtility as Locale;
-use TYPO3\CMS\Backend\Form\Container\SingleFieldContainer;
 use MageDeveloper\Dataviewer\Domain\Model\Field as Field;
-use MageDeveloper\Dataviewer\Domain\Model\FieldValue as FieldValue;
-use MageDeveloper\Dataviewer\Domain\Model\RecordValue as RecordValue;
-use TYPO3\CMS\Backend\Form\Element\AbstractFormElement as FormElement;
 use TYPO3\CMS\Backend\Form\FormDataProviderInterface;
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Tree\TableConfiguration\ExtJsArrayTreeRenderer;
+use TYPO3\CMS\Core\Tree\TableConfiguration\TableConfigurationTree;
+use TYPO3\CMS\Core\Tree\TableConfiguration\TreeDataProviderFactory;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Extbase\Object\InvalidClassException;
-use TYPO3\CMS\Extbase\Object\ObjectManager;
 
 /**
  * MageDeveloper Dataviewer Extension
@@ -25,7 +21,7 @@ use TYPO3\CMS\Extbase\Object\ObjectManager;
  * @copyright   Magento Developers / magedeveloper.de <kontakt@magedeveloper.de>
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class PrepareSelectTreeTca implements FormDataProviderInterface
+class PrepareSelectTreeTca extends \TYPO3\CMS\Backend\Form\FormDataProvider\TcaSelectTreeItems
 {
 	/**
 	 * Object Manager
@@ -36,30 +32,6 @@ class PrepareSelectTreeTca implements FormDataProviderInterface
 	protected $objectManager;
 
 	/**
-	 * Field Repository
-	 *
-	 * @var \MageDeveloper\Dataviewer\Domain\Repository\FieldRepository
-	 * @inject
-	 */
-	protected $fieldRepository;
-
-	/**
-	 * Record Repository
-	 *
-	 * @var \MageDeveloper\Dataviewer\Domain\Repository\RecordRepository
-	 * @inject
-	 */
-	protected $recordRepository;
-
-	/**
-	 * Fieldtype Settings Service
-	 * 
-	 * @var \MageDeveloper\Dataviewer\Service\Settings\FieldtypeSettingsService
-	 * @inject
-	 */
-	protected $fieldtypeSettingsService;
-
-	/**
 	 * Constructor
 	 *
 	 * @return PrepareSelectTreeTca
@@ -67,58 +39,92 @@ class PrepareSelectTreeTca implements FormDataProviderInterface
 	public function __construct()
 	{
 		$this->objectManager 			= GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\Object\ObjectManager::class);
-		$this->fieldRepository			= $this->objectManager->get(\MageDeveloper\Dataviewer\Domain\Repository\FieldRepository::class);
-		$this->recordRepository			= $this->objectManager->get(\MageDeveloper\Dataviewer\Domain\Repository\RecordRepository::class);
-		$this->fieldtypeSettingsService = $this->objectManager->get(\MageDeveloper\Dataviewer\Service\Settings\FieldtypeSettingsService::class);
 	}
-
+	
 	/**
-	 * Inline parent TCA may override some TCA of children.
+	 * Resolve select items
 	 *
 	 * @param array $result
 	 * @return array
+	 * @throws \UnexpectedValueException
 	 */
 	public function addData(array $result)
 	{
-		if($result["tableName"] == "tx_dataviewer_domain_model_record") {
-			$row        = $result["databaseRow"];
-			$fieldId 	= (int)$_GET["field"];
-			$record     = null;
-			$uid        = (isset($row["uid"]))? $row["uid"] : null;
+		$table = $result['tableName'];
 
-			if (!is_null($uid)) {
-				/* @var Record $record */
-				$record = $this->recordRepository->findByUid($uid, false);
+		foreach ($result['processedTca']['columns'] as $fieldName => $fieldConfig) {
+			if (empty($fieldConfig['config']['type']) || $fieldConfig['config']['type'] !== 'select') {
+				continue;
 			}
 
-			// We instantiate a new record model, so we can provide at least an empty model
-			if(!$record instanceof Record)
-				$record = $this->objectManager->get(Record::class);
-			
-			$field = $this->fieldRepository->findByUid($fieldId, false);
-			
-			if($field instanceof Field)
-			{
-				// We retrieve the fieldtype configuration
-				$fieldtypeConfiguration = $this->fieldtypeSettingsService->getFieldtypeConfiguration($field->getType());
-				$fieldClass             = $fieldtypeConfiguration->getFieldClass();
-			
-				if ($this->objectManager->isRegistered($fieldClass)) {
-					/* @var \MageDeveloper\Dataviewer\Form\Fieldtype\FieldtypeInterface $fieldtypeModel */
-					$fieldtypeModel = $this->objectManager->get($fieldClass);
-					$fieldtypeModel->setField($field);
-					$fieldtypeModel->setRecord($record);
+			// Make sure we are only processing supported renderTypes
+			if (!$this->isTargetRenderType($fieldConfig)) {
+				continue;
+			}
 
-					$tca = $fieldtypeModel->buildTca();
-					
-					$result['processedTca']['columns'][$fieldId] = $tca["processedTca"]["columns"][$fieldId];
+			$fieldConfig['config']['items'] = $this->sanitizeItemArray($fieldConfig['config']['items'], $table, $fieldName);
+			$fieldConfig['config']['maxitems'] = $this->sanitizeMaxItems($fieldConfig['config']['maxitems']);
+
+			$fieldConfig['config']['items'] = $this->addItemsFromSpecial($result, $fieldName, $fieldConfig['config']['items']);
+			$fieldConfig['config']['items'] = $this->addItemsFromFolder($result, $fieldName, $fieldConfig['config']['items']);
+			$staticItems = $fieldConfig['config']['items'];
+
+			$fieldConfig['config']['items'] = $this->addItemsFromForeignTable($result, $fieldName, $fieldConfig['config']['items']);
+			$dynamicItems = array_diff_key($fieldConfig['config']['items'], $staticItems);
+
+			$fieldConfig['config']['items'] = $this->removeItemsByKeepItemsPageTsConfig($result, $fieldName, $fieldConfig['config']['items']);
+			$fieldConfig['config']['items'] = $this->addItemsFromPageTsConfig($result, $fieldName, $fieldConfig['config']['items']);
+			$fieldConfig['config']['items'] = $this->removeItemsByRemoveItemsPageTsConfig($result, $fieldName, $fieldConfig['config']['items']);
+
+			$fieldConfig['config']['items'] = $this->removeItemsByUserLanguageFieldRestriction($result, $fieldName, $fieldConfig['config']['items']);
+			$fieldConfig['config']['items'] = $this->removeItemsByUserAuthMode($result, $fieldName, $fieldConfig['config']['items']);
+			$fieldConfig['config']['items'] = $this->removeItemsByDoktypeUserRestriction($result, $fieldName, $fieldConfig['config']['items']);
+
+			// Resolve "itemsProcFunc"
+			if (!empty($fieldConfig['config']['itemsProcFunc'])) {
+				$fieldConfig['config']['items'] = $this->resolveItemProcessorFunction($result, $fieldName, $fieldConfig['config']['items']);
+				// itemsProcFunc must not be used anymore
+				unset($fieldConfig['config']['itemsProcFunc']);
+			}
+
+			// Translate labels
+			$fieldConfig['config']['items'] = $this->translateLabels($result, $fieldConfig['config']['items'], $table, $fieldName);
+
+			$staticValues = $this->getStaticValues($fieldConfig['config']['items'], $dynamicItems);
+			$result['databaseRow'][$fieldName] = $this->processDatabaseFieldValue($result['databaseRow'], $fieldName);
+			// Dataviewer Edit for dataviewer fields that are numeric
+			if(!is_numeric($fieldName))
+				$result['databaseRow'][$fieldName] = $this->processSelectFieldValue($result, $fieldName, $staticValues);
+
+
+			// Keys may contain table names, so a numeric array is created
+			$fieldConfig['config']['items'] = array_values($fieldConfig['config']['items']);
+
+			// A couple of tree specific config parameters can be overwritten via page TS.
+			// Pick those that influence the data fetching and write them into the config
+			// given to the tree data provider
+			if (isset($result['pageTsConfig']['TCEFORM.'][$table . '.'][$fieldName . '.']['config.']['treeConfig.'])) {
+				$pageTsConfig = $result['pageTsConfig']['TCEFORM.'][$table . '.'][$fieldName . '.']['config.']['treeConfig.'];
+				// If rootUid is set in pageTsConfig, use it
+				if (isset($pageTsConfig['rootUid'])) {
+					$fieldConfig['config']['treeConfig']['rootUid'] = (int)$pageTsConfig['rootUid'];
 				}
-			
+				if (isset($pageTsConfig['appearance.']['expandAll'])) {
+					$fieldConfig['config']['treeConfig']['appearance']['expandAll'] = (bool)$pageTsConfig['appearance.']['expandAll'];
+				}
+				if (isset($pageTsConfig['appearance.']['maxLevels'])) {
+					$fieldConfig['config']['treeConfig']['appearance']['maxLevels'] = (int)$pageTsConfig['appearance.']['maxLevels'];
+				}
+				if (isset($pageTsConfig['appearance.']['nonSelectableLevels'])) {
+					$fieldConfig['config']['treeConfig']['appearance']['nonSelectableLevels'] = $pageTsConfig['appearance.']['nonSelectableLevels'];
+				}
 			}
 			
+			$fieldConfig['config']['treeData'] = $this->renderTree($result, $fieldConfig, $fieldName, $staticItems);
+
+			$result['processedTca']['columns'][$fieldName] = $fieldConfig;
 		}
 		
 		return $result;
 	}
-	
 }
