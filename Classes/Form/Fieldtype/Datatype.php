2123c25e1766c5ffbcdb838706de057f35884f3b
Bastian Zagar <zagar@aixdesign.net>
2017-07-10 19:40:18 +0200

    Field compatilbity

--- Classes/Form/Fieldtype/Datatype.php
+++ Classes/Form/Fieldtype/Datatype.php
@@ -14,39 +14,8 @@ use MageDeveloper\Dataviewer\Domain\Model\RecordValue;
  * @copyright   Magento Developers / magedeveloper.de <kontakt@magedeveloper.de>
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class Datatype extends AbstractFieldtype implements FieldtypeInterface
+class Datatype extends Inline
 {
-	/**
-	 * Initializes all form data providers to
-	 * $this->formDataProviders
-	 *
-	 * Will be executed in order of the added providers!
-	 *
-	 * @return void
-	 */
-	public function initializeFormDataProviders()
-	{
-		$this->formDataProviders[] = \TYPO3\CMS\Backend\Form\FormDataProvider\DatabaseRowInitializeNew::class;
-		$this->formDataProviders[] = \TYPO3\CMS\Backend\Form\FormDataProvider\TcaInlineIsOnSymmetricSide::class;
-
-		$this->formDataProviders[] = \TYPO3\CMS\Backend\Form\FormDataProvider\InlineOverrideChildTca::class;
-		$this->formDataProviders[] = \TYPO3\CMS\Backend\Form\FormDataProvider\TcaSelectTreeItems::class;
-		$this->formDataProviders[] = \TYPO3\CMS\Backend\Form\FormDataProvider\TcaInlineExpandCollapseState::class;
-		$this->formDataProviders[] = \TYPO3\CMS\Backend\Form\FormDataProvider\TcaInlineConfiguration::class;
-		$this->formDataProviders[] = \TYPO3\CMS\Backend\Form\FormDataProvider\TcaRecordTitle::class;
-		$this->formDataProviders[] = \TYPO3\CMS\Backend\Form\FormDataProvider\TcaSelectTreeItems::class;
-		$this->formDataProviders[] = \TYPO3\CMS\Backend\Form\FormDataProvider\TcaInline::class;
-
-
-		//$this->formDataProviders[] = \TYPO3\CMS\Backend\Form\FormDataProvider\TcaFlexFetch::class;
-		//$this->formDataProviders[] = \TYPO3\CMS\Backend\Form\FormDataProvider\TcaFlexPrepare::class;
-		//$this->formDataProviders[] = \TYPO3\CMS\Backend\Form\FormDataProvider\TcaFlexProcess::class;
-
-		//$this->formDataProviders[] = \MageDeveloper\Dataviewer\Form\FormDataGroup\InlineParentRecord::class;
-
-		parent::initializeFormDataProviders();
-	}
-
 	/**
 	 * Gets built tca array
 	 *
@@ -75,16 +44,16 @@ class Datatype extends AbstractFieldtype implements FieldtypeInterface
 						"label" => $this->getField()->getFrontendLabel(),
 						"config" => [
 							"type" => "inline",
-							"foreign_table" => "tx_dataviewer_domain_model_record",
+							"foreign_table" => \MageDeveloper\Dataviewer\Configuration\ExtensionConfiguration::EXTENSION_RECORD_TABLE,
 							"overrideChildTca" => [
 								"columns" => [
 									"datatype" => [
 										"config" => [
 											"default" => $this->getField()->getConfig("datatype"),
 										],
-									],		
+									],
 								],
-							],	
+							],
 							"maxitems"      => 9999,
 							"appearance" => [
 								"collapseAll" => 1,
@@ -99,41 +68,13 @@ class Datatype extends AbstractFieldtype implements FieldtypeInterface
 				],
 			],
 			"inlineStructure" => [],
-			"inlineFirstPid" => ($this->getField()->getConfig("pid_config") > 0)?(int)$this->getField()->getConfig("pid_config"):$this->getRecord()->getPid(),
+			"inlineFirstPid" => $this->getInlineFirstPid(),
 			"inlineResolveExistingChildren" => true,
 			"inlineCompileExistingChildren"=> true,
 		];
 
 		$this->prepareTca($tca);
-		
-		$this->tca = $tca;
-		return $this->tca;
+		return $tca;
 	}
-
-	/**
-	 * Prepares the TCA Array with
-	 * the form data providers
-	 *
-	 * @param array $tca
-	 */
-	public function prepareTca(array &$tca)
-	{
-		$fieldName = $tca["fieldName"];
-
-		//maxitems
-		if($maxitems = $this->getField()->getConfig("maxitems"))
-			$tca["processedTca"]["columns"][$fieldName]["config"]["maxitems"] = $maxitems;
-
-		//minitems
-		if($minitems = $this->getField()->getConfig("minitems"))
-			$tca["processedTca"]["columns"][$fieldName]["config"]["minitems"] = $minitems;
-
-		//size
-		if($size = $this->getField()->getConfig("size"))
-			$tca["processedTca"]["columns"][$fieldName]["config"]["size"] = $size;
-
-		parent::prepareTca($tca);
-	}
-
 
 }
